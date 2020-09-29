1c1fc2a534e2b31e4bf5ccc58c3574c40f342c0b
Avinash Jindal <ajavinash63@gmail.com>
2020-09-30 01:01:33 +0530

    first commit

---
+++ merge2.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import sys
+import time
+import json
+dframes=[]
+for p in sys.argv[1:]:
+   try: 
+        dframes.append(pd.read_csv(p))
+   except:
+        # print("Unable to read a file no. ",p)
+        pass
+
+for df in dframes:
+    df.columns=['id','marks']
+    df.drop_duplicates(subset=['id'], keep='last',inplace=True)
+    df.reset_index(inplace=True)
+    df.drop("index",axis=1,inplace=True)
+    df['marks'].fillna(value=0,inplace=True)
+
+mydf=[]
+
+for j in range(0,len(dframes)):
+    for i in range(0,len(dframes[j])):
+        try:
+             dframes[j]['marks'][i]=pd.to_numeric( dframes[j]['marks'][i])
+        except: 
+             mydf.append(["File" + str(j+1),dframes[j].loc[i,'id'],dframes[j].loc[i,'marks']])
+             dframes[j].drop(i,inplace=True)    
+mydf=pd.DataFrame(mydf)
+mydf.columns=['Filename','Roll No','Marks']
+log="log"+ str(time.time()) +".csv"
+# mydf.to_csv(log,mode='a',header=True,index=False)
+
+dfs = [df.set_index(['id']) for df in dframes]
+dfs = pd.concat(dfs, axis=1).reset_index()
+dfs.fillna(value=0,inplace=True)   
+result= "result"+ str(time.time()) +".csv"    
+# dfs.to_csv(result,mode='a',header=True,index=False)
+
+with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
+    print(mydf)
+    
+sys.stdout.flush()
