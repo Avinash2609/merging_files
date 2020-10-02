6d5ab4bcee101c971df20593e9535cbc62fa1013
Avinash Jindal <ajavinash63@gmail.com>
2020-10-02 07:50:17 +0530

    bugs removed

--- app.js
+++ app.js
@@ -69,25 +69,23 @@ mongoose.connect("mongodb+srv://Avinash2609:urlencoded@cluster0.qa8fk.mongodb.ne
                     var mylist=[]; 
                     mylist.push('./merge.py');
                     mylist=mylist.concat(list);
-                    res.send(mylist);
-                    // const process = spawn ('python',mylist);
-                    // process.stdout.on('data',data=>{
-                    //     res.redirect("/final")
-                        // res.send("success");
-                    // res.send(req.body.first);
-                // });
+                    // res.send(mylist);
+                    const process = spawn ('python',mylist);
+                    process.stdout.on('data',data=>{
+                        res.redirect("/final")
+                        res.send("success");
+                });
                 }
                 else if(req.body.b2){
                     var mylist=[];
                     mylist.push('./merge2.py');
                     mylist=mylist.concat(list);
-                    res.send(mylist);
-                    // const process = spawn ('python',mylist);
-                    // process.stdout.on('data',data=>{
-                    //     res.redirect("/final")
-                    // res.send(data);
-                    // res.send(req.body.second);
-                // });
+                    // res.send(mylist);
+                    const process = spawn ('python',mylist);
+                    process.stdout.on('data',data=>{
+                        res.redirect("/final")
+                    res.send(data);
+                });
                 }
             }
         });
