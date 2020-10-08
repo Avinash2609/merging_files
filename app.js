62a177940c18af3a1eca4a8449d8d9665bc8d0ff
Avinash Jindal <ajavinash63@gmail.com>
2020-10-08 19:29:22 +0530

    finally resolved

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ var imgModel = require(path.join(__dirname + '/model'));
                     mylist.push(('./merge.py'));
                     mylist=mylist.concat(list);
                     const child_process=require('child_process');
-                    const process = child_process.spawn (('./myenv/Scripts/python.exe'),mylist);
+                    const process = child_process.spawn ('python',mylist);
                     process.stdout.on('data',data=>{
                         res.send("success");
                 });
