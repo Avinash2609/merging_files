6fbc2609a255f44b68d0a0815e8247efe2fa71a5
Avinash2609 <67980248+Avinash2609@users.noreply.github.com>
2021-09-12 17:40:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -71,8 +71,8 @@ var imgModel = require(path.join(__dirname + '/model'));
                     // const process = child_process.spawn ( path.join(__dirname ,'./venv/Scripts/python'),mylist);
                     const process = child_process.spawn ("python",mylist);
                     process.stdout.on('data',data=>{
-                        res.send(data);
-                        // res.json({status: true, message: "", data:JSON.parse(data.toString())});
+//                         res.send(data);
+                        res.json({status: true, message: "", data:JSON.parse(data.toString())});
                     });
                     process.stderr.on('data', (data) => {
                     console.log(`error:${data}`);
